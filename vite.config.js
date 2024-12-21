4b60e3937a8da8cdf42d295d46d8e45777f35cc5
Luploof <139908074+Luploof@users.noreply.github.com>
2024-12-21 15:23:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://luploof.github.io/LaModa/",
+  base:"https://Luploof.github.io/LaModa/",
 })
